5bf65c6f109c907669d8934db07e6aaaeaef0f40
ObiomaIkpe <obiomaikpe1@gmail.com>
2025-07-15 19:12:43 +0100

    dockerized microservice for user auth

--- apps/auth/src/users/users.module.ts
+++ apps/auth/src/users/users.module.ts
@@ -1,4 +1,18 @@
 import { Module } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { UsersController } from './users.controller';
+import { DatabaseModule } from '@app/common';
+import { UsersRepository } from './users.repository';
+import { UserDocument, UserSchema } from './models/user.schema';
 
-@Module({})
+@Module({
+  imports: [DatabaseModule, 
+      DatabaseModule.forFeature([{name: UserDocument.name, 
+        schema: UserSchema
+      }
+  ]),
+  ],
+  controllers: [UsersController],
+  providers: [UsersService, UsersRepository]
+})
 export class UsersModule {}
